3cc2907fbf5c1ff65713e4d6b0cdaf6a0070dadc
Szewek <shevkpl@gmail.com>
2023-05-02 08:31:34 +0200

    Add validation with zod

---
+++ validate.mjs
@@ -0,0 +1,43 @@
+import { readdir, readFile } from 'node:fs/promises'
+import { join } from 'node:path'
+import { exit } from 'node:process'
+import { z } from 'zod'
+
+const luck = z.number().min(1)
+const schemas = {
+  emoji: z.record(z.object({luck, emoji: z.string().emoji()}, {description: 'Data'})),
+  letter: z.record(z.string().length(1), z.object({luck}))
+}
+
+async function validate() {
+  const cdir = join('.', 'crates')
+  const dirs = await readdir(cdir)
+  for (let dt of dirs) {
+    const dtpath = join(cdir, dt)
+    const crates = await readdir(dtpath)
+    if (!(dt in schemas)) {
+      console.error("Could not check crate type:", dt)
+      return -1
+    }
+    const cz = schemas[dt]
+    for (let cf of crates) {
+      const f = await readFile(join(dtpath, cf), 'utf8')
+      let d
+      try {
+        d = JSON.parse(f)
+      } catch (e) {
+        console.error("Invalid JSON crate file: ", cf, ";", e)
+        return 2
+      }
+      const p = await cz.safeParseAsync(d)
+      if (!p.success) {
+        console.error("Invalid data in crate file:", cf)
+        console.error(p.error.toString())
+        return 1
+      }
+    }
+  }
+  return 0
+}
+
+validate().then(c => exit(c))
